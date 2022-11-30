c69c93c81019bc1f8061aafeaab93d045672ef49
Debbie <asamoahjaeldeborah@gmail.com>
2022-11-30 11:04:33 +0000

    Created the home screen

---
+++ src/screens/home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/navbar/"
+
+const Home = () => {
+    return (
+        <div className="w-[100%] h-[100%] flex flex-col overflow-hidden ">
+            <Navbar />
+        </div>
+    )
+}
+
+export default Home

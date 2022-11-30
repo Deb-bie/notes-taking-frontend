0954d856192616037178d0cec3b30258f95eef9e
Debbie <asamoahjaeldeborah@gmail.com>
2022-11-30 11:52:30 +0000

    Made changes to the navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
-        <div className="w-[100%] h-[60px] sticky top-0 z-50 overflow-hidden ">
-            <div className="w-[100%] h-[100%] px-32 ">
+        <div className="w-[100%] h-[100%] overflow-hidden ">
+            <div className="w-[100%] h-[100%] px-32 pt-8 ">
                 <h1 className="text-4xl font-['Dancing_Script'] cursor-pointer  ">myNotes</h1>
             </div>
         </div>

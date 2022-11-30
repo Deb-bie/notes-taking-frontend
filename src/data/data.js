a07776441b9630ba72e98e1b76d389b9aa87c526
Debbie <asamoahjaeldeborah@gmail.com>
2022-11-30 11:12:44 +0000

    Created a dummy data to micmic how data will look on the screen

---
+++ src/data/data.js
@@ -0,0 +1,24 @@
+export const rows = [
+    {
+      _id: 1,
+      title: "God dey",
+      details: "Turntabl wan show me shege but God dey so I dey",
+      modified: "30/11/22 ",
+      created: "30/11/22",
+    },
+    {
+      _id: 2,
+      title: "Swimming lessons",
+      details: " I learnt how to swim today. It was actually fun ",
+      modified: "29/11/22 ",
+      created: "29/11/22 ",
+    },  
+    
+    {
+      _id: 3,
+      title: "Rock, Scissors, Paper",
+      details: "This is a new game I learnt. ",
+      modified: "28/11/22",
+      created: "28/11/22",
+    },  
+]

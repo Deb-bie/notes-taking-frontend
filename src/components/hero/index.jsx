c083d2ce3aff20b53c2f15499a3d720ae01aab54
Debbie <asamoahjaeldeborah@gmail.com>
2022-11-30 11:46:33 +0000

    Added random to see how the layout will be with data

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,15 +1,89 @@
-import {useState} from "react";
-
+import { DataGrid } from "@mui/x-data-grid";
+import {rows} from "../../data/data.js"
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline"
 
 const Hero = () => {
+
+    const columns = [
+        { field: "_id", headerName: "ID", width: 70 },
+        {
+            field: "title",
+            headerName: "Title",
+            width: 150,
+            renderCell: (params) => {
+              return (
+                <div 
+                    className="text-sky-600 underline cursor-pointer "
+                >
+                  {params.row.title}
+                </div>
+              )
+            }
+        },
+        {
+            field: "details",
+            headerName: "Details",
+            width: 250,
+        },
+        {
+            field: "modified",
+            headerName: "Last modified",
+            width: 150,
+        },
+        {
+            field: "created",
+            headerName: "Date created",
+            width: 150,
+        },
+        {
+            field: "update",
+            headerName: "Update",
+            width: 150,
+            renderCell: (params) => {
+                return (
+                    <div
+                        className="text-green-700 cursor-pointer "
+                    >
+                        <EditOutlinedIcon />
+                    </div>
+              );
+            },
+        },
+        {
+            field: "delete",
+            headerName: "Delete",
+            width: 150,
+            renderCell: (params) => {
+                return (
+                    <div 
+                        className=" text-red-400 cursor-pointer "
+                    >
+                        <DeleteOutlineIcon />
+                    </div>
+                );
+              },
+        },
+    ];
+
 
     return (
         <div className="w-[100%] h-[97vh] ">
             <div className="w-[100%] h-[100%] text-xl flex flex-col items-center content-center ">
 
-                <div className="w-[100%] h-[80vh] flex flex-col justify-center content-center items-center">
+                {/* <div className="w-[100%] h-[80vh] flex flex-col justify-center content-center items-center">
                     Sorry, you have no available notes.
-                </div>
+                </div> */}
+
+                <div className="mt-12 w-[100%] h-[80vh] px-12 p-[20px]  ">
+                    <DataGrid
+                        rows={rows}
+                        columns={columns}
+                        pageSize={9}
+                        rowsPerPageOptions={[9]}
+                        getRowId={(row) => row._id}
+                    />
+                </div>  
 
             </div>
         </div>

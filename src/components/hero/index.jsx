9b108f5bac0416c287ba40292987037a163d507f
Debbie <asamoahjaeldeborah@gmail.com>
2022-11-30 11:23:15 +0000

    Worked on how the screen will look with no available data

---
+++ src/components/hero/index.jsx
@@ -0,0 +1,19 @@
+import {useState} from "react";
+
+
+const Hero = () => {
+
+    return (
+        <div className="w-[100%] h-[97vh] ">
+            <div className="w-[100%] h-[100%] text-xl flex flex-col items-center content-center ">
+
+                <div className="w-[100%] h-[80vh] flex flex-col justify-center content-center items-center">
+                    Sorry, you have no available notes.
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Hero

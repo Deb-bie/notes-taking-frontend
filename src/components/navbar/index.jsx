a55d6778fdcac484fa6aa653fa821a7812be6ae3
Debbie <asamoahjaeldeborah@gmail.com>
2022-11-30 11:03:50 +0000

    Added the navbar component

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <div className="w-[100%] h-[60px] sticky top-0 z-50 overflow-hidden ">
+            <div className="w-[100%] h-[100%] px-32 ">
+                <h1 className="text-4xl font-['Dancing_Script'] cursor-pointer  ">myNotes</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
